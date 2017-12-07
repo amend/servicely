39196acfc21b0fefd6414b68f659a8820f6a5655
Andoni Mendoza <andoni.mendoza@gmail.com>
2017-12-07 12:35:46 -0600

    fixed bug in prepare to segu in feed

--- servicely/ChangeProfilePictureViewController.swift
+++ servicely/ChangeProfilePictureViewController.swift
@@ -22,6 +22,8 @@ class ChangeProfilePictureViewController: UIViewController, UINavigationControll
     var sourceImage: UIImage? = nil
     var imagePicker = UIImagePickerController()
     
+    var imageURL:NSURL = NSURL()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -56,12 +58,37 @@ class ChangeProfilePictureViewController: UIViewController, UINavigationControll
     @IBAction func setPicture(_ sender: Any) {
         let userID = FIRAuth.auth()?.currentUser?.uid
         let storageRef = FIRStorage.storage().reference()
+        
+        // File located on disk
+        
+        //let localFile = URL(string: "path/to/image")!
+        let localFile = URL(string: imageURL.absoluteString!)
+        
+        // Create a reference to the file you want to upload
+        let riversRef = storageRef.child("images/rivers.jpg")
+        
+        // Upload the file to the path "images/rivers.jpg"
+        let uploadTask = riversRef.putFile(localFile!, metadata: nil) { metadata, error in
+            if let error = error {
+                // Uh-oh, an error occurred!
+            } else {
+                // Metadata contains file metadata such as size, content-type, and download URL.
+                let downloadURL = metadata!.downloadURL()
+            }
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         sourceImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
         picker.dismiss(animated: true, completion: nil)
         imageView.image = sourceImage
+        
+        // get locla url of image
+        imageURL = info[UIImagePickerControllerReferenceURL] as! NSURL
+        //let imageName - imageUR
+        //let imageName = imageURL.path!.lastPathComponent
+        //let documentDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first as! String
+        //let localPath = documentDirectory.stringByAppendingPathComponent(imageName)
     }
     
     func checkPermission() {
