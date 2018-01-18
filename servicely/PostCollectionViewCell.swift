58beeb423bb3b154a542b587e5e8c53ce2542044
Andoni Mendoza <andoni.mendoza@gmail.com>
2018-01-17 18:44:59 -0600

    ui improvements to collection view cells

--- servicely/PostCollectionViewCell.swift
+++ servicely/PostCollectionViewCell.swift
@@ -45,10 +45,15 @@ class PostCollectionViewCell: UICollectionViewCell {
         super.init(coder: aDecoder)
         //fatalError("init(coder:) has not been implemented")
         
-        self.layer.borderWidth = 1
-        self.layer.cornerRadius = 2.0
-        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.backgroundColor = UIColor.white.cgColor
+        //self.layer.borderWidth = 7
+        //self.layer.cornerRadius = 15.0
         self.layer.masksToBounds = true
+        //self.layer.borderColor = UIColor.black.cgColor
+        //self.layer.borderColor = UIColor(displayP3Red: 254, green: 91, blue: 95, alpha: 1.0) as! CGColor
+        //self.layer.borderColor = CGColor.init(red: 254, green: 91, blue: 95, alpha: 1.0)
+        //self.layer.borderColor = UIColor.init(red: 254/255.0, green: 91/255.0, blue: 95/255.0, alpha: 1.0).cgColor
+            self.layer.masksToBounds = true
         
         /*
          imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height*2/3))
